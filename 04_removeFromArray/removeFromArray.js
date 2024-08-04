8ee1ec47d5f37c49fd8e25a5c908521c9cc9e38b
Stevie Peck <peckstevie204@gmail.com>
2024-08-04 00:51:45 -0400

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...remove) {
+  for (const rem of remove) {
+    array = array.filter((arg) => arg !== rem);
+  }
+  return array;
 };
 
 // Do not edit below this line
