176708281d92433ddba0b0084842d0dc0e56de60
Stevie Peck <peckstevie204@gmail.com>
2024-08-04 01:27:47 -0400

    Refactor exercise 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,10 @@
 const sumAll = function (num1, num2) {
-  if (
-    !Number.isInteger(num1) ||
-    !Number.isInteger(num2) ||
-    num1 < 1 ||
-    num2 < 1
-  ) {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  } else if (num1 < 1 || num2 < 1) {
     return "ERROR";
   }
+
 
   const terms = Math.abs(num2 - num1) + 1;
   return (terms / 2) * (num1 + num2);
