34486bf7cf65593e5f2507ad3314724259777409
Stevie Peck <peckstevie204@gmail.com>
2024-08-04 01:23:37 -0400

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 1 ||
+    num2 < 1
+  ) {
+    return "ERROR";
+  }
 
+  const terms = Math.abs(num2 - num1) + 1;
+  return (terms / 2) * (num1 + num2);
 };
 
 // Do not edit below this line
