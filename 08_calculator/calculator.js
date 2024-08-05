fd0240f75cfb9f2759b4497cd25057870c893a80
Stevie Peck <peckstevie204@gmail.com>
2024-08-05 16:18:12 -0400

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, num) => (sum += num), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((product, num) => (product *= num), 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  arr = Array(num).fill().map((_, index) => index + 1);
+  return multiply(arr);
 };
 
 // Do not edit below this line
@@ -29,5 +30,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
