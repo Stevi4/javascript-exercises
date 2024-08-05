e02629846a40e74c627320aab05219d9e55e6063
Stevie Peck <peckstevie204@gmail.com>
2024-08-05 17:55:55 -0400

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const getAge = (person) => {
+    if ("yearOfDeath" in person) {
+      return person["yearOfDeath"] - person["yearOfBirth"];
+    } else {
+      return new Date().getFullYear() - person["yearOfBirth"];
+    }
+  };
 
+  return people.reduce((oldest, person) => {
+    if (getAge(person) > getAge(oldest)) {
+      return person;
+    }
+    return oldest;
+  }, people[0]);
 };
 
 // Do not edit below this line
