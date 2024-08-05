8a6838b277966d1d59db1ebdac2add02e47c032b
Stevie Peck <peckstevie204@gmail.com>
2024-08-05 16:59:42 -0400

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const chars = str
+    .toLowerCase()
+    .split("")
+    .filter(
+      (char) => (char >= "a" && char <= "z") || (char >= "0" && char <= "9")
+    );
+  const reverse = [...chars].reverse();
+  return chars.every((char, index) => (char === reverse[index]));
 };
 
 // Do not edit below this line
