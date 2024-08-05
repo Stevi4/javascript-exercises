58bf183e02572f03852c8c5ea63de6657801ae60
Stevie Peck <peckstevie204@gmail.com>
2024-08-05 17:27:07 -0400

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n == 0) {
+    return 0;
+  } else if (n < 0) {
+    return "OOPS";
+  }
 
+  let current = 1;
+  let prev = 1;
+  for (let i = 0; i < n - 2; i++) {
+    let next = current + prev;
+    prev = current;
+    current = next;
+  }
+  return current;
 };
 
 // Do not edit below this line
